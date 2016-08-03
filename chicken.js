562053f827e159afda829eeed82294497cc1219e
scaactk <tjut_ljx@yeah.net>
2016-08-03 22:45:41 +0800

    js random

---
+++ chicken.js
@@ -0,0 +1,23 @@
+﻿var data="Is gay,Not a gay,Don't know";
+var arr=data.split(",");
+var time;
+
+function start(){
+  clearInterval(time);
+  document.getElementById("showResult").value="";
+  time = setInterval('change()',50);
+}
+
+function change(){
+  document.getElementById("ran").innerText = arr[ran()];
+}
+
+function ran(){
+  return parseInt(Math.random()*3);
+}
+
+function stop(){
+  clearInterval(time);
+  document.getElementById("ran").innerText = "Is gay";
+  document.getElementById("showResult").value = document.getElementById("ran").innerText;
+}
---
+++ random.js
@@ -0,0 +1,21 @@
+﻿var alldata = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,,s,t,u,v,w,x,y,z";
+var alldataarr = alldata.split(",");
+var num = alldataarr.length ;
+var timer;
+
+function start(){
+  clearInterval(timer);
+  timer = setInterval('change()',100);
+}
+
+function change(){
+  document.getElementById("oknum").innerHTML = alldataarr[GetRnd(num)];
+}
+
+function GetRnd(num){
+  return parseInt(Math.random()*num);//26段一个单位的长度
+}
+function ok(){
+  clearInterval(timer);
+  document.getElementById("showresult").value = document.getElementById("oknum").innerText;
+}
